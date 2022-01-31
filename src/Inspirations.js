76525a640463d5a88f3a11225d89798c321c90b7
KubaMach667 <90350478+KubaMach667@users.noreply.github.com>
2022-02-01 00:46:29 +0100

    Update Inspirations.js

--- src/Inspirations.js
+++ src/Inspirations.js
@@ -16,13 +16,13 @@ export default function Inspirations({language}) {
         In 6 months 1/2 of this list could be completely different as there is an infinite ammount
         of music left for me to discover. I see the first 4 rows (particularly top 10) as monoliths of magnitude
         I can't even dream reaching. Most of albums on this list changed how
-        I percieved not only art but also politics, philosophy and myself. - kuba
+        I percieved not only art but also politics and philosophy. - kuba
         <br></br>  <br></br> if the photos are illegible just open them in another tab/download them, they are hi-res  </div> }
         {language=="english" && <div> Płyty/filmy które są moimi ulubionymi i wywarły na mnie największy wpływ. Powiedziałbym, że top 15 jest stałe
        a reszta zmienia się średnio co tydzień ze względu na mój humor oraz to czego słuchałem ostatnio.
       Za pół roku połowa tej listy może się kompletnie zmienić, ponieważ pozostała mi nieskończona ilość muzyki do odkrycia.
       Pierwsze cztery rzędy (szczególnie top 10) to monolity tak wielkie, że nawet nie potrafię sobie nawet wyobrazić 
-       wejścia na taki poziom artystyczny. Większość albumów na tej liście zmieniło nie tylko to jak odbieram sztukę, ale również politykę, filozofię oraz siebie. - kuba
+       wejścia na taki poziom artystyczny. Większość albumów na tej liście zmieniło nie tylko to jak odbieram sztukę, ale również politykę oraz filozofię. - kuba
         <br></br>  <br></br> jeżeli zdjęcia są nieczytelne, polecam odpalić w nowej karcie/pobrać, są one w wysokiej rozdzielczości  </div> }
        </div>
         <div><img src={InspirationMusic} className="Topster"></img></div>
